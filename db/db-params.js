aaeba1ce62e7b574799fe6a7e4a4b9b71a3becd4
Martin Grigorov <Air@Martin.local>
2017-11-17 21:58:30 +0200

    Add heroku params for the db

--- db/db-params.js
+++ db/db-params.js
@@ -2,5 +2,5 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/BurgesApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/BurgesApp');
 module.exports = {mongoose};
